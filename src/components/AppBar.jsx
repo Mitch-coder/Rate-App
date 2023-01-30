77e6d56c5122fe63439ab7a04d1586089094b9a9
Mitch-Coder <michellescalderon2002@gmail.com>
2023-01-30 15:38:52 -0600

    test5

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
         paddingTop:Constants.statusBarHeight + 10,
         paddingBottom:10,
         paddingLeft:10
-    },
+    },            
     active:{
         color:theme.appBar.textPrimary
     },
